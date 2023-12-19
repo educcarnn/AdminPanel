dd3a15bd119bbc00db42e2032c4d275d834bb361
educcarnn <educcarnn@gmail.com>
2023-12-19 19:51:19 -0300

    feat: initial structure project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
